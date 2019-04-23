5970f3151d0c6f1f7c7326c63ad00ea2791ac3b9
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-23 21:31:27 +0100

    Add TextBox component to App Component

--- Week-1/index.js
+++ Week-1/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import Image from '~/src/Image.js'
 import { products } from '~/constants/Products.js';
+import TextBox from '~/src/Textbox.js'
 
 class App extends Component {
   render() {
     let options = { width: 200, height: 150, alt: ''}
     return (
       <div>
+        <TextBox title="List of Products:"/>
         {
           products.map((product) => (
             <Image key={product.id}
