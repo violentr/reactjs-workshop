d124dc48225568e7eb5177e88aa726ced4504bbf
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-24 19:00:46 +0100

    Fix relative component path

--- Week-1/src/Image.js
+++ Week-1/src/Image.js
@@ -1,4 +1,4 @@
-import { products } from '~/constants/Products.js';
+import { products } from '../constants/Products.js';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 const Image = (props) => {
