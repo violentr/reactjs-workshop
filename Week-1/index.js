16c265a23cb06c96cd10904c9459b695fd46f516
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-24 18:37:33 +0100

    Add Price Component to App

--- Week-1/index.js
+++ Week-1/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import Image from '~/src/Image.js'
 import { products } from '~/constants/Products.js';
 import TextBox from '~/src/Textbox.js'
+import Price from '~/src/Price.js'
 
 class App extends Component {
   render() {
     let options = { width: 200, height: 150, alt: ''}
     return (
       <div>
+        <Price price="32"/>
         <TextBox title="List of Products:"/>
         {
           products.map((product) => (
