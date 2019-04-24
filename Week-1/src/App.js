a59d016546e99cda9acf8152659963d1526460c9
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-24 20:06:08 +0100

    Add List products with ProductCard on main page

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import Image from './Image.js'
 import { products } from '../constants/Products.js';
-import TextBox from './Textbox.js'
-import Price from './Price.js'
+import ProductCard from './ProductCard.js'
 
 class App extends Component {
   render() {
     let options = { width: 200, height: 150, alt: ''}
     return (
       <div>
-        <Price price="32"/>
-        <TextBox title="List of Products:"/>
         {
           products.map((product) => (
-            <Image key={product.id}
+            <ProductCard key={product.id}
               {...product} {...options}
             />
           ))
