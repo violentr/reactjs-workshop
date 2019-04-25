4b4e39d8c0f68bb207169477ef54649db1af9794
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-25 20:53:52 +0100

    Add Render Image component with props

--- Week-1/index.js
+++ Week-1/index.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
-import helpers from '~/src/Helper.js'
+import Image from '~/src/Image.js'
+import { products } from '~/constants/Products.js';
 
 class App extends Component {
   render() {
-    let { add, substract, multiply, divide } = helpers
+    let options = { width: 200, height: 150, alt: ''}
     return (
       <div>
-        Hello World!
-        <p> 3 + 4 = { add(3, 4) } </p>
-        <p> 5 + 3 = { substract(5, 3) }</p>
-        <p> 5 * 3 = { multiply(5, 3) }</p>
-        <p> 6 / 4 = { divide(6, 4) }</p>
+        {
+          products.map((product) => (
+            <Image key={product.id}
+              {...product} {...options}
+            />
+          ))
+        }
       </div>
     );
   }
--- Week-1/src/Image.js
+++ Week-1/src/Image.js
@@ -2,11 +2,10 @@ import { products } from '~/constants/Products.js';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 const Image = (props) => {
-  let {title, imageUrl, width, hight, alt } = props
-
+  let {title, imageUrl, width, height, alt } = props
   return(
     <div>
-      <img style={{ width: width, height: hight}} src={ imageUrl }
+      <img style={{ width: width, height: height}} src={ imageUrl }
         alt={alt == '' ? title : alt }
       />
       <span> { title } </span>
