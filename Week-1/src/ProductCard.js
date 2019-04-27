059bc905ebcc2809e160fa7b024b5c9d39245c18
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-27 17:44:10 +0100

    Add AddToBasket component to ProductCard

--- Week-1/src/ProductCard.js
+++ Week-1/src/ProductCard.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Image from './Image.js'
 import TextBox from './Textbox.js'
 import Price from './Price.js'
+import { default as BasketButton} from './AddToBasket.js'
 
 const ProductCard = (props) =>(
   <div>
     <Image {...props} />
     <TextBox title={ props.title }/>
     <Price price={ props.price } />
+    <BasketButton />
   </div>
 )
 
