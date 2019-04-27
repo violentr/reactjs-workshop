d46acc9fb12ff65213d34c2d7d8e9cb5e881418b
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-04-27 17:23:12 +0100

    Add button, Busket with total items as 0

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -3,9 +3,15 @@ import { products } from '../constants/Products.js'
 import Catalog from './Catalog.js'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {counter: 0}
+  }
+
   render() {
     return (
       <div>
+        <button> Basket {this.state.counter} </button>
         <Catalog products={ products } />
       </div>
     );
