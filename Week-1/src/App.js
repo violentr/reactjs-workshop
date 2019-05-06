92ba19e24ae6d782b604e7eed42c13d24270ab6c
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-06 14:01:52 +0100

    Add Simple navigation, links

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import CartContainer from '~/containers/CartContainer.js'
 import Catalog from '~/src/Catalog.js'
 import { products } from '~/constants/Products.js'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
 import { mainPath, aboutPath, productPath, productsPath } from '~/src/routes/helpers.js'
 import Product from '~/src/components/views/Product/index.js'
 import Main from '~/src/components/views/Main/index.js'
@@ -11,22 +11,35 @@ import Products from '~/src/components/views/Products/index.js'
 import NotFound from '~/src/components/views/NotFound/index.js'
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path={ aboutPath() } exact strict component={About} />
-      <Route path={ mainPath() }  exact strict component={Main} />
-      <Route path={ productsPath() } exact strict component={Products} />
-      <Route path={ productPath() } render={ ({ match }) => (
-        <Product id={match.params.id} />
-      )} />
+  <Fragment>
+    <Router>
+      <ul>
+        <li>
+          <NavLink to={mainPath()}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={productsPath()}>Products</NavLink>
+        </li>
+        <li>
+          <NavLink to={aboutPath()}>About</NavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route path={ aboutPath() } exact strict component={About} />
+        <Route path={ mainPath() }  exact strict component={Main} />
+        <Route path={ productsPath() } exact strict component={Products} />
+        <Route path={ productPath() } render={ ({ match }) => (
+          <Product id={match.params.id} />
+        )} />
       <Route path="*" exact strict component={NotFound} />
     </Switch>
-    <Fragment>
-      <CartContainer>
-        <Catalog products={ products } />
-      </CartContainer>
-    </Fragment>
-  </Router>
+      <Fragment>
+        <CartContainer>
+          <Catalog products={ products } />
+        </CartContainer>
+      </Fragment>
+    </Router>
+  </Fragment>
 )
 
 export default App;
