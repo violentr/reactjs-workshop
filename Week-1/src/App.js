fabd48a781232c15b453f582457d5d354e7683fe
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-06 12:11:39 +0100

    Add Simple routes with render function
    main, about products, product, not found
    inline JSX markup

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -2,12 +2,21 @@ import React, { Fragment } from 'react'
 import CartContainer from '~/containers/CartContainer.js'
 import Catalog from '~/src/Catalog.js'
 import { products } from '~/constants/Products.js'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 const App = () => (
-  <Fragment>
-    <CartContainer>
-      <Catalog products={ products } />
-    </CartContainer>
-  </Fragment>
+  <Router>
+    <Switch>
+      <Route path="/about" exact strict render={() => <div> About page</div>} />
+      <Route path="/" exact strict render={() => <div> Main page </div>} />
+      <Route path="/products" exact strict render={() => <div> Products page</div>} />
+      <Route path="/products/:id" exact strict render={() => <div> Product page</div>} />
+      <Route path="*" exact strict render={() => <div> Page not found </div>} />
+    </Switch>
+    <Fragment>
+      <CartContainer>
+        <Catalog products={ products } />
+      </CartContainer>
+    </Fragment>
+  </Router>
 );
 export default App;
