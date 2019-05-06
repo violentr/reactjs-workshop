74276e0bd737467434bf3c5150d9437b7a3d98ea
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-06 12:27:19 +0100

    Add Route helpers for pages

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -3,13 +3,15 @@ import CartContainer from '~/containers/CartContainer.js'
 import Catalog from '~/src/Catalog.js'
 import { products } from '~/constants/Products.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { mainPath, aboutPath, productPath, productsPath } from '~/src/routes/helpers.js'
+
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/about" exact strict render={() => <div> About page</div>} />
-      <Route path="/" exact strict render={() => <div> Main page </div>} />
-      <Route path="/products" exact strict render={() => <div> Products page</div>} />
-      <Route path="/products/:id" exact strict render={() => <div> Product page</div>} />
+      <Route path={ aboutPath() } exact strict render={() => <div> About page</div>} />
+      <Route path={ mainPath() }  exact strict render={() => <div> Main page </div>} />
+      <Route path={ productsPath() } exact strict render={() => <div> All Products page</div>} />
+      <Route path={ productPath() } exact strict render={() => <div> Product page</div>} />
       <Route path="*" exact strict render={() => <div> Page not found </div>} />
     </Switch>
     <Fragment>
@@ -18,5 +20,6 @@ const App = () => (
       </CartContainer>
     </Fragment>
   </Router>
-);
+)
+
 export default App;
---
+++ Week-1/src/routes/helpers.js
@@ -0,0 +1,4 @@
+export const mainPath = () => '/'
+export const productsPath = () => '/products'
+export const productPath = (id = ':id') => `/products/${id}`
+export const aboutPath = () => '/about'
