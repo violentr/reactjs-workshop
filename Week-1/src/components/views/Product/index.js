1ab29bd0ea8953884be2a61e052329bf8833aece
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-06 20:27:43 +0100

    Add Individual Product page, with its attrs
    /products/:id

--- Week-1/src/components/views/Product/index.js
+++ Week-1/src/components/views/Product/index.js
@@ -1,8 +1,14 @@
 import React, { Fragment } from 'react'
-
+import ProductCard from '~/src/ProductCard.js'
+import { products } from '~/constants/Products.js'
+const selectProduct = (id) => {
+  let options = { width: 200, height: 150, alt: ''}
+  let item = products.filter((item) => item.id == id )[0]
+  return <ProductCard {...item} {...options}/>
+}
 const Product = ({ id }) => (
   <Fragment>
-    <h3> Product Page #{ id } </h3>
+    { selectProduct(id) }
   </Fragment>
 )
 
