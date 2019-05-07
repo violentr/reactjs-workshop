81378381218391b431cd5cd83112554ccb260626
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-07 20:46:32 +0100

    Add TextBox component has only needed props

--- Week-1/src/ProductCard/Textbox.js
+++ Week-1/src/ProductCard/Textbox.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { productPath } from '~/src/routes/helpers.js'
 
-const TextBox = (props) =>(
-  <Link to={productPath(props.id)}>
-    {props.title}
+const TextBox = ({ id, title}) =>(
+  <Link to={productPath(id)}>
+    {title}
   </Link>
 )
 
