87cdc72e07f85abc92255ca8681d6bfab0383d4c
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-09 21:08:03 +0100

    Add Checkout page contains list of selected products

--- Week-1/src/components/views/Checkout/index.js
+++ Week-1/src/components/views/Checkout/index.js
@@ -1,9 +1,25 @@
 import React, { Fragment } from 'react'
 
-const Checkout = ({items}) => (
+import Description from '~/src/ProductCard/Description.js'
+const totalCost = (products) =>{
+  let total = 0
+  products.map((product) => total += product.price)
+  return total
+}
+const Checkout = ({ history, location, match }) => {
+  let products = location.state.products
+  let options = { width: 150, height: 100, alt: ''}
+  console.log("products", products.map(product => product.title))
+  return(
   <Fragment>
-    <h3> This is Checkout page #{items}</h3>
+    <h3> This is Checkout page </h3>
+    { products.map((product, i) => (
+      <Description key={ i } product={ product } style={ options }/>)
+      )
+    }
+    <p> Total : { totalCost(products) } $</p>
   </Fragment>
-)
+  )
+}
 
 export default Checkout
