4671ebe6ab0542cffc142f76bb0d41d8f9b933fd
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-09 22:31:18 +0100

    Add List selected products on checkout page, total amount

--- Week-1/src/components/views/Checkout/index.js
+++ Week-1/src/components/views/Checkout/index.js
@@ -1,25 +1,30 @@
 import React, { Fragment } from 'react'
+import Product from '~/src/ProductCard/Product.js'
+import NotFoundPage from '~/src/components/views/NotFound/index.js'
 
-import Description from '~/src/ProductCard/Description.js'
 const totalCost = (products) =>{
   let total = 0
   products.map((product) => total += product.price)
   return total
 }
-const Checkout = ({ history, location, match }) => {
-  let products = location.state.products
+const listProducts = (products) => {
   let options = { width: 150, height: 100, alt: ''}
-  console.log("products", products.map(product => product.title))
+  return products.map((product, i) => (
+    <Product key={ i } product={ product } style={ options }/>)
+  )
+
+}
+const CheckoutPage = ({ history, location, match }) => {
+  let products = location.state ? location.state.products : <NotFoundPage />
+
   return(
   <Fragment>
     <h3> This is Checkout page </h3>
-    { products.map((product, i) => (
-      <Description key={ i } product={ product } style={ options }/>)
-      )
-    }
-    <p> Total : { totalCost(products) } $</p>
+    {location.state ? listProducts(products) : <NotFoundPage />}
+
+    { location.state ? (<p> Total $ {totalCost(products)} </p>) : '' }
   </Fragment>
   )
 }
 
-export default Checkout
+export default CheckoutPage
