86dafcb676c93baecb6be1a66eb46b015830646a
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-17 07:15:06 +0100

    Add get list of products by calling api, render it
    componentDidMount lifecycle

--- Week-1/src/components/Catalog.js
+++ Week-1/src/components/Catalog.js
@@ -1,24 +1,45 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import Product from '~/src/ProductCard/Product.js'
 import AddToBasket from '~/src/ProductCard/AddToBasket.js'
+import request from 'superagent'
+const APIURL = "https://my-json-server.typicode.com/violentr/fake_api/products"
 
-const Catalog = (props) => {
-  let { products} = props
-  let options = { width: 200, height: 150, alt: ''}
-
-  return (
-    <div>
-      {
-       products.map((product, i) => (
-         <Fragment key={ i }>
-           <Product key={product.id}
-             product={product} style={options}
-           />
-           <AddToBasket key={ i } product={product} />
-         </Fragment>
-        ))
-      }
-    </div>
+const fetchData = () => {
+  return(
+    request.get(APIURL)
+      .then(results => results.body)
+      .catch(console.error)
   )
+}
+class Catalog extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {products: []}
+  }
+
+  componentDidMount(){
+    fetchData().then(products => {
+      this.setState({products: products})
+    })
+  }
+
+  render() {
+    let options = { width: 200, height: 150, alt: ''}
+    return (
+      <div>
+        {
+          this.state.products && this.state.products.map((product, i) => (
+            <Fragment key={ i }>
+              <Product key={product.id}
+                product={product} style={options}
+              />
+              <AddToBasket key={ i } product={product} />
+            </Fragment>
+          ))
+        }
+      </div>
+    )
+  }
 }
 export default Catalog
