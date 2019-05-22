2a81eaccd20bc18879127222cf28d757cfc1ab55
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-22 18:01:16 +0100

    Add Used productContext to populate list of products

--- Week-1/src/components/views/Product/index.js
+++ Week-1/src/components/views/Product/index.js
@@ -1,23 +1,25 @@
 import React, { Fragment } from 'react'
 import Product from '~/src/ProductCard/Product.js'
-import { products } from '~/src/constants/Products.js'
 import NotFoundPage from '~/src/components/views/NotFound/index.js'
 import {ProductConsumer} from '~/src/context/ProductContext.js'
 
-const selectProduct = (id) => {
+const renderProduct = (product) => {
   let options = { width: 200, height: 150, alt: ''}
-  let item = products.filter((item) => item.id == id )[0]
-   return (
-    <ProductConsumer>
-      {(cart) =>
-        <Fragment >
-          <button onClick={() => console.log(cart)}> Products  </button>
-        </Fragment>
-      }
-    </ProductConsumer>
-
- ) //return item ? <Product product={item} style={options}/> : <NotFoundPage />
+  return product ? <Product product={product} style={options}/> : <NotFoundPage /> 
 }
+
+const selectProduct = (id) => {  
+  return (
+  <ProductConsumer>
+    {(productContext) =>
+      <Fragment>
+        { renderProduct(productContext.find_by_id(productContext.products, id)) }
+      </Fragment>
+    }
+  </ProductConsumer>
+ )
+}
+
 const ProductPage = ({ id }) => (
   <Fragment>
       { selectProduct(id) }
--- Week-1/src/components/views/Products/index.js
+++ Week-1/src/components/views/Products/index.js
@@ -1,36 +1,19 @@
 import React, { Fragment, Component } from 'react'
 import Catalog from '~/src/components/Catalog.js'
-import { products } from '~/src/constants/Products.js'
 import request from 'superagent'
-const APIURL = "https://my-json-server.typicode.com/violentr/fake_api/products"
-import {ProductProvider} from '~/src/context/ProductContext.js'
+import {ProductConsumer} from '~/src/context/ProductContext.js'
 
-const fetchData = () => {
-  return(
-    request.get(APIURL)
-      .then(results => results.body)
-      .catch(console.error)
-  )
-}
 
 class ProductsPage extends Component {
-  constructor(props){
-    super(props)
-    this.state = {products: []}
-  }
-
-  componentDidMount(){
-    fetchData().then(products => {
-      this.setState({products: products})
-    })
-  }
-
   render(){
     return(
-      <Fragment>
-        <ProductProvider value={this.state.products} />
-        <Catalog products={ products } />
-      </Fragment>
+      <ProductConsumer>
+      {productContext =>
+        <Fragment>
+          <Catalog products={ productContext.products } />
+        </Fragment>
+      }
+      </ProductConsumer>
     )
   }
 }
