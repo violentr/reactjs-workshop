1e7ed2490bd701b47df61d89f76033b6c8e26d73
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-26 14:14:56 +0100

    Add Image validated with prop-types

--- Week-1/src/components/ProductCard/Image.js
+++ Week-1/src/components/ProductCard/Image.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Image = ( {title, imageUrl, alt, style }) => {
-  let {height, width} = style
-
+const Image = ({title, imageUrl, alt, style}) => {
   return(
     <div>
-      <img style={{ width, height}} src={ imageUrl }
-        alt={alt ? title : alt }
+      <img style={style} src={imageUrl}
+        alt={alt ? title : alt}
       />
     </div>
   )
+}
+Image.propTypes = {
+    title: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+    imageUrl: PropTypes.string.isRequired,
+  style: PropTypes.shape({
+    height: PropTypes.number.isRequired,
+    width:  PropTypes.number.isRequired
+  }).isRequired
 }
 export default Image
