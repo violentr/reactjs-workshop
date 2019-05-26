605078f88e94cdf7689db799ed41b66c7e5b2b68
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-26 14:15:55 +0100

    Add Product validated with prop-types

--- Week-1/src/components/ProductCard/Product.js
+++ Week-1/src/components/ProductCard/Product.js
@@ -2,21 +2,34 @@ import React from 'react'
 import Image from './Image.js'
 import TextBox from '~/src/components/ProductCard/Textbox.js'
 import Price from '~/src/components/ProductCard/Price.js'
+import PropTypes from 'prop-types'
 
-const Product = ({ product, history, style }) =>{
-  let { height, width} = style
+const Product = ({product, style}) =>{
   return(
     <div>
       <Image
-        title={ product.title }
-        alt={ product.alt }
-        imageUrl={ product.imageUrl }
-        style={ style }
+        title={product.title}
+        alt={product.alt}
+        imageUrl={product.imageUrl}
+        style={style}
       />
       <TextBox title={product.title} id={product.id} />
-      <Price price={ product.price } />
+      <Price price={product.price} />
     </div>
   )
+}
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    extra_images: PropTypes.array.isRequired
+  }),
+  style: PropTypes.shape({
+    height: PropTypes.number.isRequired,
+    width:  PropTypes.number.isRequired
+  })
 }
 
 export default Product
