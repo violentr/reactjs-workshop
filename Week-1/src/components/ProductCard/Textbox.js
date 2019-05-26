9c6d169a839e6bea1c342720a8d33eff901238b2
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-26 14:16:43 +0100

    Add TextBox validated with prop-types

--- Week-1/src/components/ProductCard/Textbox.js
+++ Week-1/src/components/ProductCard/Textbox.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { productPath } from '~/src/routes/helpers.js'
+import PropTypes from 'prop-types'
 
-const TextBox = ({ id, title}) =>(
+const TextBox = ({id, title}) =>(
   <Link to={productPath(id)}>
     {title}
   </Link>
 )
-
+TextBox.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+}
 export default TextBox
