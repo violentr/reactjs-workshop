7959ae1ad3ec93515137df74cf76f1e46d17dbbb
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-27 19:44:51 +0100

    Add prepareData, make sure data is available on routes

--- Week-1/src/routes/Product.js
+++ Week-1/src/routes/Product.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { productPath } from '~/src/routes/helpers.js'
 import ProductPage from '~/src/components/views/Product/index.js'
+import {fetchProduct} from '~/src/actions/Product.js'
 
 export default {
   path: productPath(),
   render: ({ match }) => (
     <ProductPage id={match.params.id} />
+  ),
+  prepareData: (store, query, params) => (
+    store.dispatch(fetchProduct(params.id))
   )
 }
