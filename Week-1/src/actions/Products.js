df2446e25450043cd4b36bc9615bb1a7b76ee359
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-27 17:26:19 +0100

    Add Product and Products actions

---
+++ Week-1/src/actions/Products.js
@@ -0,0 +1,26 @@
+import * as ProductsActionTypes from '~/src/actiontypes/Products.js'
+import request from 'superagent'
+import {API_URL} from '~/src/constants/Api.js'
+
+const requestProducts = () => ({
+  type: ProductsActionTypes.FETCH_PRODUCTS_SUCCESS
+})
+
+const receiveProducts = (response) => ({
+  type: ProductsActionTypes.FETCH_PRODUCTS_SUCCESS,
+  response
+})
+
+const errorProducts = () => ({
+  type: ProductsActionTypes.FETCH_PRODUCTS_ERROR,
+})
+
+export const fetchProducts = () => (
+  (dispatch) => {
+    dispatch(requestProducts())
+    request.get(API_URL)
+      .end((error, response) => {
+        error ? dispatch(errorProducts()) : dispatch(receiveProducts(response.body))
+      })
+  }
+)
