fe548ba170fab2d719227f97fbae909f5ea7f4a2
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-05-27 17:28:08 +0100

    Add Redux store to the App, integration

--- Week-1/src/App.js
+++ Week-1/src/App.js
@@ -1,15 +1,19 @@
-import React, { Fragment } from 'react'
+import React, {Fragment} from 'react'
 import Layout from '~/src/components/Layout.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import ProductContainer from '~/src/containers/ProductContainer.js'
+import {Provider} from 'react-redux'
+import store from '~/src/store'
 
 const App = () => (
   <Fragment>
-      <Router>
-        <ProductContainer>
-          <Layout />
-        </ProductContainer>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <ProductContainer>
+            <Layout />
+          </ProductContainer>
+        </Router>
+      </Provider>
   </Fragment>
 )
 
