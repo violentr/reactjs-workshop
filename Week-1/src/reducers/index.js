6077f0575cf40e8274bd866d9d87bdd289b54f01
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-02 10:38:39 +0100

    Add Basket reducer to list of reducers

--- Week-1/src/reducers/index.js
+++ Week-1/src/reducers/index.js
@@ -2,5 +2,6 @@ import {combineReducers} from 'redux'
 
 import product from '~/src/reducers/Product.js'
 import products from '~/src/reducers/Products.js'
+import basket from '~/src/reducers/Basket.js'
 
-export default combineReducers({products, product})
+export default combineReducers({products, product, basket})
