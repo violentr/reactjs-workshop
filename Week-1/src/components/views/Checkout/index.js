9eacc1a00321456a18ab0dcb0d3625193f416ccb
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-03 22:01:43 +0100

    Add Fix CheckoutPage use Redux store instead of context

--- Week-1/src/components/views/Checkout/index.js
+++ Week-1/src/components/views/Checkout/index.js
@@ -1,35 +1,44 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import ProductCard from '~/src/components/Product/Card.js'
 import { imageAttributes } from '~/src/shared/helper.js'
-import { BasketConsumer } from '~/src/context/BasketContext.js'
+import {connect} from 'react-redux'
 
-const listProducts = (context) => {
-  let { basket, totalCost} = context
-  let products = basket.map((product, i) => (
+const listProducts = (props) => {
+  let { items, totalCost} = props
+  let products = items.map((product, i) => (
     <ProductCard key={i} product={product} style={imageAttributes}/>)
   )
   return(
     <Fragment>
       { products.length > 0 && products }
-      <p> Total: $ { totalCost(basket) } </p>
+      <p> Total: $ { totalCost()} </p>
     </Fragment>
   )
 }
-const CheckoutPage = () => (
-  <Fragment>
-    <BasketConsumer >
-      {
-        (cartContext) =>{
-          return(
-          <Fragment>
-            <h3> This is Checkout page </h3>
-            { listProducts(cartContext) }
-          </Fragment>
-          )
-        }
-      }
-    </BasketConsumer>
-  </Fragment>
-)
 
-export default CheckoutPage
+class CheckoutPage extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <Fragment>
+        <Fragment>
+          <h3> This is Checkout page </h3>
+          { listProducts(this.props) }
+        </Fragment>
+      </Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state) => (
+  { items: state.basket.items,
+    totalCost: () => {
+      let total = 0
+      state.basket.items.map((product) => total += product.price)
+      return total
+    }
+  }
+)
+export default connect(mapStateToProps)(CheckoutPage)
