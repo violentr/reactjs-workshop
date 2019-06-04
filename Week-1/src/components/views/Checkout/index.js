6e6274740aa8b005697f53e1362fef0cf58ebc54
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-04 20:18:59 +0100

    Remove totalCost from props, made as Component func
    No need to have this logic available through the
    props make it as regular component func

--- Week-1/src/components/views/Checkout/index.js
+++ Week-1/src/components/views/Checkout/index.js
@@ -1,44 +1,36 @@
-import React, { Fragment, Component } from 'react'
+import React, {Fragment, Component} from 'react'
 import ProductCard from '~/src/components/Product/Card.js'
-import { imageAttributes } from '~/src/shared/helper.js'
+import {imageAttributes} from '~/src/shared/helper.js'
 import {connect} from 'react-redux'
 
-const listProducts = (props) => {
-  let { items, totalCost} = props
-  let products = items.map((product, i) => (
-    <ProductCard key={i} product={product} style={imageAttributes}/>)
-  )
-  return(
-    <Fragment>
-      { products.length > 0 && products }
-      <p> Total: $ { totalCost()} </p>
-    </Fragment>
-  )
-}
-
 class CheckoutPage extends Component {
+
   constructor(props){
     super(props)
   }
+
+  totalCost(items){
+    let total = 0
+    items.map((product) => total += product.price)
+    return total
+  }
+
   render(){
+    let {items} = this.props
+    let products = items.map((product, i) => (
+      <ProductCard key={i} product={product} style={imageAttributes}/>)
+    )
     return (
       <Fragment>
-        <Fragment>
-          <h3> This is Checkout page </h3>
-          { listProducts(this.props) }
-        </Fragment>
+        <h3> This is Checkout page </h3>
+        {products}
+        <p> Total: $ {this.totalCost(items)} </p>
       </Fragment>
     )
   }
 }
 
 const mapStateToProps = (state) => (
-  { items: state.basket.items,
-    totalCost: () => {
-      let total = 0
-      state.basket.items.map((product) => total += product.price)
-      return total
-    }
-  }
+  {items: state.basket.items}
 )
 export default connect(mapStateToProps)(CheckoutPage)
