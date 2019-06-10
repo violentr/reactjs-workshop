16dfde37b42cb9b8e2dac83c96e78a72766e7c41
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-10 22:24:04 +0100

    Add Save to localStorage as soon as state updated

--- Week-1/src/store/index.js
+++ Week-1/src/store/index.js
@@ -2,7 +2,17 @@ import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducers from '~/src/reducers'
 import DevTools from '~/src/containers/DevTools.js'
+import {saveState} from '~/src/shared/cartPersistence.js'
+
 const store = createStore(reducers, compose(applyMiddleware(thunk), DevTools.instrument()))
+
+store.subscribe(() => {
+  let items = store.getState().basket.items
+
+  if (items.length > 0){
+    saveState(items)
+  }
+});
 
 export default store
 
