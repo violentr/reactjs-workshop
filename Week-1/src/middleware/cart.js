3beb0fcd34d0dc758920dbba5c9fa9b2b8a31669
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-15 17:40:33 +0100

    Add car middleware, load items from local storage

---
+++ Week-1/src/middleware/cart.js
@@ -0,0 +1,8 @@
+import {loadState} from '~/src/shared/cartPersistence.js'
+
+export default (store) => (next) => (action) => {
+  if (!action.type == 'LOAD_CART') return next(action)
+  const items = loadState()
+  let result = Object.assign({}, action, {items})
+  return next(result)
+}
