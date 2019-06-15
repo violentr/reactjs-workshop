ed14699a4d686a54ee038648f5db30fc027d80d7
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-15 11:24:03 +0100

    Add action creator addToStorage

--- Week-1/src/actions/Basket.js
+++ Week-1/src/actions/Basket.js
@@ -5,8 +5,19 @@ const addToBasket = (product) => ({
   product
 })
 
+const addToStorage = (storedItems) => ({
+  type: BasketActionTypes.LOAD_CART,
+  storedItems
+})
+
+
 export const addProductToBasket = (product) => (
   (dispatch) => {
     dispatch(addToBasket(product))
+  }
+)
+export const addProductsToStorage = (products) => (
+  (dispatch) => {
+    dispatch(addToStorage(products))
   }
 )
