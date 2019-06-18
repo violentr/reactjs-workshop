399507e5b2fe1579223706845090a50597ab7816
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-18 20:59:48 +0100

    Add Submit customer order
    On submit aler box showin JSON
    current order with customer details

--- Week-1/src/components/views/Checkout/index.js
+++ Week-1/src/components/views/Checkout/index.js
@@ -3,20 +3,42 @@ import ProductCard from '~/src/components/Product/Card.js'
 import {connect} from 'react-redux'
 import CheckoutForm from '~/src/components/views/Checkout/Form.js'
 
+const totalCost = (items) => {
+  return items.reduce((total, product) => total + product.price, 0)
+}
+
 class CheckoutPage extends Component {
 
   constructor(props){
     super(props)
-  }
 
-  totalCost(items){
-    return items.reduce((total, product) => total + product.price, 0)
+    this.order = {
+      products: [],
+      totalItems: null,
+      totalCost: null
+    }
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
     let {form} = this.props
-    alert(JSON.stringify(form))
+    let customer = {customer: form}
+    this.currentOrder()
+
+    alert(JSON.stringify(Object.assign({}, customer, this.order)))
+    console.log(JSON.stringify(Object.assign({}, customer, this.order)))
+  }
+
+  currentOrder(){
+    let {items} = this.props
+
+    if (items.length > 0) {
+      items.map((item) =>(
+        this.order.products.push([item.title, item.price])
+      ))
+      this.order.totalItems = items.length
+      this.order.totalCost = totalCost(items)
+    }
   }
 
   render(){
@@ -28,18 +50,17 @@ class CheckoutPage extends Component {
       <Fragment>
         <h3> This is Checkout page </h3>
         {products}
-        <p> Total: $ {this.totalCost(items)} </p>
-        <CheckoutForm handleSubmit={this.handleSubmit}/>
+        <p> Total: $ {totalCost(items)} </p>
+        { items.length > 0 && <CheckoutForm handleSubmit={this.handleSubmit}/> }
       </Fragment>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-console.log("form", state.form)
-return  {
-  items: state.basket.items,
-  form: state.form.checkout && state.form.checkout.values
+const mapStateToProps = (state) => (
+  {
+    items: state.basket.items,
+    form: state.form.checkout && state.form.checkout.values
   }
-}
+)
 export default connect(mapStateToProps)(CheckoutPage)
