ac228866413ec90f56c4066f316219dd32d31e07
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-22 18:55:41 +0100

    Add webpack common settings used in diff envs

---
+++ Week-1/initializers/webpack/common.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: path.resolve(process.cwd(), 'public', 'assets'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: [
+          path.resolve(process.cwd(), 'src')
+        ],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader', options: {sourceMap: true}},
+          {loader: 'css-loader', options: {sourceMap: true}}
+        ],
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve(process.cwd(), 'src'),
+      'node_modules'
+    ]
+  }
+}
--- Week-1/initializers/webpack/development.js
+++ Week-1/initializers/webpack/development.js
@@ -1,40 +1,13 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const common = require('./common.js')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
+
   devServer: {
     contentBase: path.resolve(process.cwd(), 'public'),
     historyApiFallback: true
-  },
-  entry: 'index.js',
-  output: {
-    path: path.resolve(process.cwd(), 'public', 'assets'),
-    publicPath: '/assets/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        include: [
-          path.resolve(process.cwd(), 'src')
-        ],
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {loader: 'style-loader', options: {sourceMap: true}},
-          {loader: 'css-loader', options: {sourceMap: true}}
-        ],
-      }
-    ]
-  },
-  resolve: {
-    modules: [
-      path.resolve(process.cwd(), 'src'),
-      'node_modules'
-    ]
   }
-}
+})
+
