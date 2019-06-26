419f3e21b17a45701b4dbfbd397c6d3a510e8146
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-06-26 20:09:56 +0100

    Make stateless component Catalog

--- Week-1/src/components/views/Products/Catalog.js
+++ Week-1/src/components/views/Products/Catalog.js
@@ -1,28 +1,22 @@
-import React, {Fragment, Component} from 'react'
+import React, {Fragment} from 'react'
 import ProductCard from 'components/Product/Card.js'
 import ProductAddToBasket from 'components/Product/AddToBasket.js'
 import {connect} from 'react-redux'
 
-class Catalog extends Component {
-
-  render(){
-    let {products} = this.props
-    return (
-      <div>
-        {
-          products && products.map((product, i) => (
-            <Fragment key={i}>
-              <ProductCard key={product.id}
-                product={product}
-              />
-              <ProductAddToBasket key={i} product={product} />
-            </Fragment>
-          ))
-        }
-      </div>
-    )
-  }
-}
+const Catalog = ({products}) => (
+  <div>
+    {
+      products && products.map((product, i) => (
+        <Fragment key={i}>
+          <ProductCard key={product.id}
+            product={product}
+          />
+          <ProductAddToBasket key={i} product={product} />
+        </Fragment>
+      ))
+    }
+  </div>
+)
 
 const mapStateToProps = (state) => ({
   products: state.products.entries
